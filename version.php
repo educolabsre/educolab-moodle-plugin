98a9992e515b424daaed294f96308d829b4628ca
educolabsre <educolabsre@gmail.com>
2026-08-06 17:51:30 -0300

    Versão 0.1.4

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_educolab';
-$plugin->release = '0.1.3';
-$plugin->version = 2026052102;
+$plugin->release = '0.1.4';
+$plugin->version = 2026080601;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
